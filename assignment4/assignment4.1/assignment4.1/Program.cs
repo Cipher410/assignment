d71f307c03adf512f5be5776b299df0c37df02dd
Cipher410 <2899269571@qq.com>
2025-03-19 16:45:02 +0800

    Add files via upload

---
+++ assignment4/assignment4.1/assignment4.1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+// 定义泛型链表节点类
+class Node<T>
+{
+    public T Data { get; set; }
+    public Node<T> Next { get; set; }
+
+    public Node(T data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+// 定义泛型链表类
+class GenericLinkedList<T>
+{
+    private Node<T> Head { get; set; }
+
+    // 添加元素到链表
+    public void Add(T data)
+    {
+        Node<T> newNode = new Node<T>(data);
+        if (Head == null)
+        {
+            Head = newNode;
+        }
+        else
+        {
+            Node<T> current = Head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+
+    // 实现 ForEach 方法
+    public void ForEach(Action<T> action)
+    {
+        Node<T> current = Head;
+        while (current != null)
+        {
+            action(current.Data); // 对每个元素执行传入的 action
+            current = current.Next;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 创建一个整数链表
+        GenericLinkedList<int> list = new GenericLinkedList<int>();
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+        list.Add(40);
+        list.Add(50);
+
+        // 打印链表元素
+        Console.WriteLine("链表元素:");
+        list.ForEach(x => Console.WriteLine(x));
+
+        // 求最大值
+        int max = int.MinValue;
+        list.ForEach(x => { if (x > max) max = x; });
+        Console.WriteLine($"最大值: {max}");
+
+        // 求最小值
+        int min = int.MaxValue;
+        list.ForEach(x => { if (x < min) min = x; });
+        Console.WriteLine($"最小值: {min}");
+
+        // 求和
+        int sum = 0;
+        list.ForEach(x => sum += x);
+        Console.WriteLine($"和: {sum}");
+    }
+}
---
+++ assignment4/assignment4.2/assignment4.2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading;
+
+// 定义闹钟类
+class AlarmClock
+{
+    // 定义 Tick 事件
+    public event EventHandler Tick;
+    // 定义 Alarm 事件
+    public event EventHandler Alarm;
+
+    // 闹钟的当前时间
+    public DateTime CurrentTime { get; private set; }
+    // 闹钟的响铃时间
+    public DateTime AlarmTime { get; set; }
+
+    // 构造函数
+    public AlarmClock()
+    {
+        CurrentTime = DateTime.Now;
+    }
+
+    // 启动闹钟
+    public void Start()
+    {
+        Console.WriteLine("闹钟已启动！");
+        while (true)
+        {
+            // 更新当前时间
+            CurrentTime = DateTime.Now;
+            Console.WriteLine($"当前时间: {CurrentTime:HH:mm:ss}");
+
+            // 触发 Tick 事件
+            OnTick();
+
+            // 检查是否到达响铃时间
+            if (CurrentTime >= AlarmTime)
+            {
+                // 触发 Alarm 事件
+                OnAlarm();
+                break; // 响铃后停止闹钟
+            }
+
+            // 模拟每秒嘀嗒一次
+            Thread.Sleep(1000);
+        }
+    }
+
+    // 触发 Tick 事件
+    protected virtual void OnTick()
+    {
+        Tick?.Invoke(this, EventArgs.Empty);
+    }
+
+    // 触发 Alarm 事件
+    protected virtual void OnAlarm()
+    {
+        Alarm?.Invoke(this, EventArgs.Empty);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 创建闹钟实例
+        AlarmClock alarmClock = new AlarmClock();
+
+        // 设置响铃时间（当前时间 + 10 秒）
+        alarmClock.AlarmTime = DateTime.Now.AddSeconds(10);
+
+        // 订阅 Tick 事件
+        alarmClock.Tick += (sender, e) =>
+        {
+            Console.WriteLine("嘀嗒...");
+        };
+
+        // 订阅 Alarm 事件
+        alarmClock.Alarm += (sender, e) =>
+        {
+            Console.WriteLine("叮铃铃！时间到了！");
+        };
+
+        // 启动闹钟
+        alarmClock.Start();
+    }
+}
